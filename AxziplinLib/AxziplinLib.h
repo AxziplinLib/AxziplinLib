0af24fdb1c5efbb4b61e5c4caf8f84fc7596c20e
devedbox <862099730@qq.com>
2017-05-05 19:56:18 +0800

    Initial Commit

---
+++ AxziplinLib/AxziplinLib.h
@@ -0,0 +1,19 @@
+//
+//  AxziplinLib.h
+//  AxziplinLib
+//
+//  Created by devedbox on 2017/5/5.
+//  Copyright © 2017年 jiangyou. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AxziplinLib.
+FOUNDATION_EXPORT double AxziplinLibVersionNumber;
+
+//! Project version string for AxziplinLib.
+FOUNDATION_EXPORT const unsigned char AxziplinLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AxziplinLib/PublicHeader.h>
+
+
